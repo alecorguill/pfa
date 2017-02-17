fb72ee17c17fa9ee1f0a86a5ac45eaa0448a2406
DUARTE Anthony <aduarte001@pororoca.pedago.ipb.fr>
2017-02-17 13:55:32 +0100

    first

---
+++ neoblik/WebContent/WEB-INF/jeux.jsp
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <title>Jeux à venir</title>
+</head>
+<body>
+<h1>Jeux à venir..</h1>
+D'autres jeux seront implémentés au fur et à mesure de notre avancement. Merci de vous tenir informés !!
+
+<p><a href="choix_jeu">Retour aux choix du jeu</a></p>
+</body>
+</html>
---
+++ neoblik/src/neoblik/ChoixJeu.java
@@ -0,0 +1,42 @@
+package neoblik;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+
+import beans.Player;
+import model.Players;
+import form.FormPlayer;
+
+public class ChoixJeu extends HttpServlet {
+
+	public static final String VUE = "/WEB-INF/choix_jeu.jsp";	
+    public static final String CHAMP_PSEUDO = "pseudo";
+	
+	public void doGet( HttpServletRequest request, HttpServletResponse response )   throws ServletException, IOException {
+		
+		this.getServletContext().getRequestDispatcher( VUE ).forward( request, response );
+	}
+	
+    public void doPost( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException{
+        /* Traitement des données du formulaire */
+    	String pseudo = request.getParameter( CHAMP_PSEUDO );
+
+    	FormPlayer form = new FormPlayer();
+    	
+    	Player player = form.signPlayer(request);
+    	
+    	HttpSession session = request.getSession();
+    	session.setAttribute("player", player);
+    	ServletContext context = this.getServletContext();
+    	Players p = (Players) context.getAttribute("players");
+    	p.addPlayer(player);
+    	
+        this.getServletContext().getRequestDispatcher( VUE ).forward( request, response );
+    	
+    }
+    
+}
